e57bf3750cee4769b5b1050e76a0d79c09009a73
Siddhant <siddhantahire98@gmail.com>
2025-11-19 12:34:05 +0530

    feat: update project structure to include routing with react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </Router>
     <App />
   </StrictMode>,
 )
